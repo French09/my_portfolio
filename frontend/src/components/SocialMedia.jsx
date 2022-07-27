db0609b6646196d8ad0733f33293a352d63d8489
French09 <francois.dinong@epitech.eu>
2022-07-27 11:34:44 +0200

    :art: Add Github btn AppWrapper tweak

--- frontend/src/components/SocialMedia.jsx
+++ frontend/src/components/SocialMedia.jsx
@@ -9,6 +9,8 @@ const SocialMedia = () => {
           <a href='https://www.linkedin.com/in/francois-dinong/' target="_blank" rel="noreferrer">
             <BsLinkedin width="50px"/>
           </a>
+        </div>
+        <div className='app__social'>
           <a href='https://github.com/French09?tab=repositories' target="_blank" rel="noreferrer">
             <BsGithub width="50px"/>
           </a>
