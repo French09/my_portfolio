2e970b3eb621bb5db8c872789cf14c8fd53d2346
Frenchua09 <francois-paul1.dinong@epitech.eu>
2023-09-29 16:06:54 +0200

    Contact form work

--- frontend/src/container/About/About.jsx
+++ frontend/src/container/About/About.jsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
-import { urlFor, client } from '../../client'
-import { AppWrap } from '../../wrapper'
-import { motion } from 'framer-motion'
-import './About.scss'
+import { urlFor, client } from "../../client";
+import { AppWrap } from "../../wrapper";
+import { motion } from "framer-motion";
+import "./About.scss";
 
 const About = () => {
   const [abouts, setAbouts] = useState([]);
@@ -11,34 +11,38 @@ const About = () => {
   useEffect(() => {
     const query = '*[_type == "abouts"]';
 
-    client.fetch(query)
-      .then((data) => { setAbouts(data); });
-  }, [])
+    client.fetch(query).then((data) => {
+      setAbouts(data);
+    });
+  }, []);
 
   return (
     <>
-        <h2 id="moi" className="head-text">
-          Apprenti développeur à la recherche d'une <span>alternance</span>
-        </h2>
+      <h2 id="moi" className="head-text">
+        Apprenti développeur à la recherche d'une <span>alternance</span>
+      </h2>
 
-        <div className='app__profiles'>
-          {abouts.map((about, index) => (
-            <motion.div
-              whileInView={{ opcaity: 1 }}
-              whileHover={{ scale: 1.1 }}
-              transition={{ duration: 0.5, type: 'tween' }}
-              className='app__profile-item'
-              key={about.title + index}
-            >
-              <img src={urlFor(about.imgUrl)} alt={about.title} />
-              <h2 className='bold-text' style={{ marginTop: 20 }} >{about.title}</h2>
-              <p className='p-text' style={{ marginTop: 10 }} >{about.description}</p>
-            </motion.div>
-          ))}
+      <div className="app__profiles">
+        {abouts.map((about, index) => (
+          <motion.div
+            whileInView={{ opcaity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.5, type: "tween" }}
+            className="app__profile-item"
+            key={about.title + index}
+          >
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
+            <h2 className="bold-text" style={{ marginTop: 20 }}>
+              {about.title}
+            </h2>
+            <p className="p-text" style={{ marginTop: 10 }}>
+              {about.description}
+            </p>
+          </motion.div>
+        ))}
       </div>
-
     </>
-  )
-}
+  );
+};
 
-export default AppWrap(About, 'about')
+export default AppWrap(About, "about");
--- frontend/src/container/Footer/Footer.jsx
+++ frontend/src/container/Footer/Footer.jsx
@@ -3,128 +3,103 @@ import emailjs from '@emailjs/browser';
 
 import { images } from '../../constants'
 import { AppWrap, MotionWrap } from '../../wrapper'
-import { client } from '../../client'
 import './Footer.scss'
 
 const Footer = () => {
+	const form = useRef();
 
-  const [formData, setFormData] = useState({ name: '', email: '', message: '' });
-  const [isFormSubmitted, setisFormSubmitted] = useState(false);
-  const [loading, setLoading] = useState(false);
+	const [formData, setFormData] = useState({ name: '', email: '', message: '' });
+	const [isFormSubmitted, setisFormSubmitted] = useState(false);
+	const [loading, setLoading] = useState(false);
 
-  // Déstructure la variable formData dans les variable "name", "email, "message"
-  const { name, email, message } = formData
+	// Déstructure la variable formData dans les variable "name", "email, "message"
+	const { name, email, message } = formData
 
-  const handleChangeInput = (e) => {
-    const { name, value } = e.target
+	const handleChangeInput = (e) => {
+		const { name, value } = e.target
 
-    setFormData({ ...formData, [name]: value })
-  }
+		setFormData({ ...formData, [name]: value })
+	}
 
-  // const handleSubmit = () => {
-  //   setLoading(true);
+	const sendEmail = (e) => {
+		e.preventDefault();
 
-  //   const contact = {
-  //     _type: 'contact',
-  //     name: 'formData.name',
-  //     email: email,
-  //     message: message
-  //   }
+		emailjs.sendForm('service_1wndcpm', 'template_1vi6b9h', form.current, 'qebYwCEj0leylm8H0')
+			.then((result) => {
+				setLoading(true);
+				setTimeout(() => {
+					console.log(result.text);
+					setisFormSubmitted(true);
+				}, 2000)
+			}, (error) => {
+				console.log(error.text);
+			});
+	};
 
-  //   client.create(contact)
-  //     .then(() => {
-  //       setLoading(false);
-  //       setisFormSubmitted(true);
-  //     })
-  // }
+	return (
+		<>
+			<h2 id="contact" className='head-text'>Contactez-moi</h2>
 
-  const form = useRef();
+			<div className='app__footer-cards'>
+				<div className='app__footer-card'>
+					<img src={images.email} alt="email" />
+					<a href="mailto: francoispaul.dinong@gmail.com"
+						className='p-text'
+					>francoispaul.dinong@gmail.com</a>
+				</div>
+				<div className='app__footer-card'>
+					<img src={images.mobile} alt="mobile" />
+					<a href="tel: +33 6 34 45 68 58"
+						className='p-text'
+					>+33 6 34 45 68 58</a>
+				</div>
+			</div>
 
-  const sendEmail = (e) => {
-    e.preventDefault();
+			{!isFormSubmitted ? (
+				<div className='app__footer-form app__flex'>
+					<form ref={form} onSubmit={sendEmail}>
 
-    emailjs.sendForm('service_t5cev1e', 'template_kg5lb2q', form.current, '32D4ig6qr_lKkKAnQ')
-      .then((result) => {
-        setLoading(true);
-        setTimeout(() => {
-          console.log(result.text);
-          setisFormSubmitted(true);
-        }, 2000)
-      }, (error) => {
-        console.log(error.text);
-      });
-  };
-
-  return (
-    <>
-      <h2 id="contact" className='head-text'>Contactez-moi</h2>
-
-      <div className='app__footer-cards'>
-        <div className='app__footer-card'>
-          <img src={images.email} alt="email" />
-          <a href="mailto: francoispaul.dinong@gmail.com"
-            className='p-text'
-          >francoispaul.dinong@gmail.com</a>
-        </div>
-        <div className='app__footer-card'>
-          <img src={images.mobile} alt="mobile" />
-          <a href="tel: +33 6 34 45 68 58"
-            className='p-text'
-          >+33 6 34 45 68 58</a>
-        </div>
-      </div>
-
-      {!isFormSubmitted ? (
-        <div className='app__footer-form app__flex'>
-          <form ref={form} onSubmit={sendEmail}>
-
-            <div className='app__flex'>
-              <input
-                className='p-text'
-                type="text"
-                placeholder="Votre nom"
-                name='name'
-                value={name}
-                onChange={handleChangeInput}
-              />
-            </div>
-            <div className='app__flex'>
-              <input
-                className='p-text'
-                type="email"
-                placeholder="Votre email"
-                name='email'
-                value={email}
-                onChange={handleChangeInput}
-              />
-            </div>
-            <div className='app__flex'>
-              <textarea
-                className='p-text'
-                placeholder='Votre message'
-                value={message}
-                name="message"
-                onChange={handleChangeInput}
-              />
-            </div>
-            <button
-              type='submit'
-              className='p-text'
-            // onClick={handleSubmit}
-            >
-              {!loading ? 'Envoyer' : 'En cour d\'envoi...'}
-            </button>
-          </form>
-        </div>
-      ) : (
-        <div>
-          <h3 className='head-text'>
-            Merci pour votre prise de contact
-          </h3>
-        </div>
-      )}
-    </>
-  )
+						<div className='app__flex'>
+							<input
+								className='p-text'
+								type="text"
+								placeholder="Votre nom"
+								name='name'
+								value={name}
+								onChange={handleChangeInput}
+							/>
+						</div>
+						<div className='app__flex'>
+							<input
+								className='p-text'
+								type="email"
+								placeholder="Votre email"
+								name='email'
+								value={email}
+								onChange={handleChangeInput}
+							/>
+						</div>
+						<div className='app__flex'>
+							<textarea
+								className='p-text'
+								placeholder='Votre message'
+								value={message}
+								name="message"
+								onChange={handleChangeInput}
+							/>
+						</div>
+						<input type="submit" value="Send" />
+					</form>
+				</div>
+			) : (
+				<div>
+					<h3 className='head-text'>
+						Merci pour votre prise de contact
+					</h3>
+				</div>
+			)}
+		</>
+	)
 }
 
 export default AppWrap(MotionWrap(Footer, 'app__footer'), 'contact', 'app__whitebg')
